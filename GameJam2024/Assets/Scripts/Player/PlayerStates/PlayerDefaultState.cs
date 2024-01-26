99990c21ae31db377a7576c41b51586f2d7a57f0
2000krysztof <2000krzysztof@gmail.com>
2024-01-26 23:08:51 +0000

    add camera movement and scaling

--- GameJam2024/Assets/Scripts/Player/PlayerStates/PlayerDefaultState.cs
+++ GameJam2024/Assets/Scripts/Player/PlayerStates/PlayerDefaultState.cs
@@ -6,20 +6,5 @@ public class PlayerDefaultState : PlayerBaseState{
 
 
 
-	public void Move(PlayerBehaviour behaviour,Vector2 direction){
-				
-	}
-
-	public void Fire(PlayerBehaviour behaviour){
-
-	}
-	public void Interact(PlayerBehaviour behaviour){
-
-	}
-
-	public virtual void Look(PlayerBehaviour behaviour, Vector2 lookDirection){
-
-	}
-
 }
 
